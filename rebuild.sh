6e31abd243fb02f268d0c0f0aa2cab761bffe118
Filip Bulanda <filipbulanda00@gmail.com>
2026-08-03 08:59:55 +0200

    Final version of rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -7,5 +7,4 @@ echo "NixOS Rebuilding ..."
 sudo nixos-rebuild switch &>/tmp/nixos-switch.log || (
   cat /tmp/nixos-switch.log | grep --color error && false)
 git commit -am "NixOS rebuild $(date '+%Y-%m-%d %H:%M')"
-git push origin main
 popd
